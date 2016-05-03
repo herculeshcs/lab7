8c1a5446aebcae319496f75809df8942e30b88ed
root <root@usa.redes.lab.ic.unicamp.br>
2016-05-03 16:39:33 -0300

    mudando tipo para float (a gpu da intel não suporta double :'( )

--- lab7.cpp
+++ lab7.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include <CL/opencl.h>
 #define MAXSOURCE 2048
 #define MAX_DEVICE_NAME_SIZE 100
@@ -20,18 +21,42 @@
 "        c[id] = a[id] + b[id];                                  \n" \
 "}                                                               \n" \
                                                                 "\n" ;
-                                                                */
+
+
+
+ */
+void print_opencl_error(FILE* fh, cl_int err)
+{
+#define PRINT_ERR(code) case code : fprintf(fh, #code); break
+  switch(err) {
+    PRINT_ERR(CL_INVALID_PROGRAM);
+    PRINT_ERR(CL_INVALID_VALUE);
+    PRINT_ERR(CL_INVALID_DEVICE);
+    PRINT_ERR(CL_INVALID_BINARY);
+    PRINT_ERR(CL_INVALID_BUILD_OPTIONS);
+    PRINT_ERR(CL_INVALID_OPERATION);
+    PRINT_ERR(CL_COMPILER_NOT_AVAILABLE);
+    PRINT_ERR(CL_BUILD_PROGRAM_FAILURE);
+    PRINT_ERR(CL_OUT_OF_RESOURCES);
+    PRINT_ERR(CL_OUT_OF_HOST_MEMORY);
+  default:
+    fprintf(fh, "unknown code");
+    break;
+  };
+  return;
+}
+                                                               
  
 int main( int argc, char* argv[] )
 {
     // Length of vectors
-    unsigned int n = 100000;
+    unsigned int n = 10;
  
     // Host input vectors
-    double *h_a;
-    double *h_b;
+    float *h_a;
+    float *h_b;
     // Host output vector
-    double *h_c;
+    float *h_c;
     
     char *kernelSource = (char *) malloc(MAXSOURCE*sizeof(char));
     // Open kernel file
@@ -58,12 +83,12 @@ int main( int argc, char* argv[] )
  	cl_platform_id* platforms;   
  	    cl_uint platformCount;
     // Size, in bytes, of each vector
-    size_t bytes = n*sizeof(double);
+    size_t bytes = n*sizeof(float);
  
     // Allocate memory for each vector on host
-    h_a = (double*)malloc(bytes);
-    h_b = (double*)malloc(bytes);
-    h_c = (double*)malloc(bytes);
+    h_a = (float*)malloc(bytes);
+    h_b = (float*)malloc(bytes);
+    h_c = (float*)malloc(bytes);
  
     // Initialize vectors on host
     int i;
@@ -105,20 +130,36 @@ int main( int argc, char* argv[] )
  
     // Create a context  
     context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
- 	
+    printf("criando contexto %d\n",err);
     // Create a command queue 
     queue = clCreateCommandQueue(context, device_id, 0, &err);
  
     // Create the compute program from the source buffer
     program = clCreateProgramWithSource(context, 1,
                             (const char **) & kernelSource,(const size_t *) &source_size, &err);
- 
+   printf("erro criando proma %d\n",err);
     // Build the program executable 
-    clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
- 
+
+    err = clBuildProgram(program, 0,NULL, NULL, NULL, NULL);
+    if (err == CL_BUILD_PROGRAM_FAILURE) {
+        cl_int logStatus;
+        char* buildLog = NULL;
+        size_t buildLogSize = 0;
+        logStatus = clGetProgramBuildInfo (program, device_id, CL_PROGRAM_BUILD_LOG, buildLogSize, buildLog, &buildLogSize);
+        buildLog = (char*)malloc(buildLogSize);
+        memset(buildLog, 0, buildLogSize);
+        logStatus = clGetProgramBuildInfo (program, device_id, CL_PROGRAM_BUILD_LOG, buildLogSize, buildLog, NULL);
+        printf("%s", buildLog);
+        free(buildLog);
+        return err;
+    } else if (err) {
+        printf("Unknown build program error !!!! (%d)\n", err);
+        return err;
+    }
+    printf("erro buildprogram %d",err);
     // Create the compute kernel in the program we wish to run
     kernel = clCreateKernel(program, "vecAdd", &err);
- 
+   printf("erro criando kernel %d\n",err);
     // Create the input and output arrays in device memory for our calculation
     d_a = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
     d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
@@ -129,16 +170,20 @@ int main( int argc, char* argv[] )
                                    bytes, h_a, 0, NULL, NULL);
     err |= clEnqueueWriteBuffer(queue, d_b, CL_TRUE, 0,
                                    bytes, h_b, 0, NULL, NULL);
- 
+    printf("erro copiando buffers %d\n",err);
     // Set the arguments to our compute kernel
     err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
     err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
     err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
     err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &n);
- 
+    printf("erro set args %d\n",err);
     // Execute the kernel over the entire range of the data set  
-    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize,
-                                                              0, NULL, NULL);
+    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
+	  fprintf(stderr, "Error: clEnqueueNDRangeKernel(..) returned error code %d (", err);
+       print_opencl_error(stderr, err);
+       fprintf(stderr, ")\n");
+
+    printf("erro chama kernel: %d , globalsize = %d localsize = %d \n",err,globalSize,localSize);
  
     // Wait for the command queue to get serviced before reading back results
     clFinish(queue);
@@ -148,10 +193,13 @@ int main( int argc, char* argv[] )
                                 bytes, h_c, 0, NULL, NULL );
  
     //Sum up vector c and print result divided by n, this should equal 1 within error
-    double sum = 0;
+    float sum = 0;
     for(i=0; i<n; i++)
+	{
+	printf("h_c= %d",h_c[i]);
         sum += h_c[i];
-    printf("final result: %f\n", sum/n);
+	}
+    printf("final result: %f %d \n", sum/n,n);
  
     // release OpenCL resources
     clReleaseMemObject(d_a);
